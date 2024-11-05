cd3a005efe476b0a8431171d5a5982243d61fb93
Alvina004 <gabrielyanalvina2@gmail.com>
2024-11-05 20:25:04 +0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,10 @@
-
 def parse_exp(expression):
     tokens = [] 
     num = ""   
     for char in expression:
         if char.isdigit() or char == '.': 
             num += char 
-        elif char in '+-*/': 
+        elif char in '+-*/%()': 
             if num:
                 tokens.append(float(num)) 
                 num = "" 
@@ -20,21 +19,23 @@ def parse_exp(expression):
 
     return tokens
 
-def calculator(tokens):
-    i = 0
-    while i < len(tokens):
-        if tokens[i] == '*' or tokens[i] == '/':
+def evaluate(tokens):
+      i = 0
+      while i < len(tokens):
+        if tokens[i] == '*' or tokens[i] == '/' or tokens[i] == '%':
             if tokens[i] == '*':
                 result = tokens[i-1] * tokens[i+1]
             elif tokens[i] == '/':
                 result = tokens[i-1] / tokens[i+1]
+            elif tokens[i] == '%':
+                result = tokens[i-1] % tokens[i+1]
             
             tokens[i-1:i+2] = [result]
         else:
             i += 1
 
-    i = 0
-    while i < len(tokens):
+      i = 0
+      while i < len(tokens):
         if tokens[i] == '+' or tokens[i] == '-':
             if tokens[i] == '+':
                 result = tokens[i-1] + tokens[i+1]
@@ -45,9 +46,26 @@ def calculator(tokens):
         else:
             i += 1
 
-    return tokens[0]
+      return tokens[0]
 
-expression = input("Enter a mathematical expression (for example: 12+896-742/58*365): ")
+def calculator(tokens):
+    stack = []
+    sub_expr = []
+
+    for token in tokens:
+        if token == '(':
+            stack.append(sub_expr)
+            sub_expr = []
+        elif token == ')':
+            result = evaluate(sub_expr)
+            sub_expr = stack.pop()
+            sub_expr.append(result)
+        else:
+            sub_expr.append(token)
+
+    return evaluate(sub_expr)
+
+expression = input("Enter a mathematical expression (for example: 12*4+(56-11)%4+23): ")
 tokens = parse_exp(expression)
 result = calculator(tokens)
-print("Result:", result)  
+print("Result:", result)
